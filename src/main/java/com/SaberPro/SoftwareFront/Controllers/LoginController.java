93ab86b31dc5dfa10281d52a9da924659822ec36
JudRom <jdromero@unillanos.edu.co>
2025-05-04 22:58:53 -0500

    Fix to CRUD section

--- src/main/java/com/SaberPro/SoftwareFront/Controllers/LoginController.java
+++ src/main/java/com/SaberPro/SoftwareFront/Controllers/LoginController.java
@@ -8,10 +8,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Base64;
 import java.util.Map;
-
+import java.util.Scanner;
 
 public class LoginController {
     @FXML
@@ -19,7 +26,6 @@ public class LoginController {
 
     @FXML
     private PasswordField passwordField;
-
 
     @FXML
     private void onLoginButtonClick() {
@@ -30,7 +36,7 @@ public class LoginController {
             // Enviar la solicitud y obtener la respuesta
             HttpResponse<String> response = BuildRequest.getInstance().POSTJson(
                     "http://localhost:8080/SaberPro/usuario/login",
-                    String.format("{\"nombreUsuario\":\"%s\", \"password\":\"%s\"}", username, password));
+                     String.format("{\"nombreUsuario\":\"%s\", \"password\":\"%s\"}", username, password));
 
             // Manejar la respuesta
             int responseCode = response.statusCode();
@@ -70,5 +76,9 @@ public class LoginController {
             System.out.println("Error al conectar con el backend.");
         }
     }
+    @FXML
+    private void onForgotPasswordClick() {
+        Stage stage = (Stage) usernameField.getScene().getWindow();
+        ViewLoader.loadView("Recovery-view.fxml", stage, 450, 480);
+    }
 }
-
